9cbb25cb25df5d6a34887e7a020260e7245bf6f7
harriszhang <harriszhang@live.cn>
2019-11-19 10:04:47 +0800

    update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,14 +10,38 @@ export class GameComponent implements OnInit {
 
     constructor() { }
 
+    should: number;
     items: Item[];
 
     ngOnInit() {
+        this.should = 1;
         this.items = [];
         for (var i = 0; i < 25; i++) {
             var item = new Item();
             item.number = i + 1;
             this.items.push(item);
         }
+
+        this.shuffle(this.items);
+    }
+
+    onClick(item: Item) {
+        if (this.should !== item.number) {
+            return;
+        }
+        item.selected = true;
+        this.should++;
+    }
+
+    shuffle = arr => {
+        arr.forEach((element, index) => {
+            const randomIndex = Math.floor(Math.random() * (index + 1))
+            this.swap(arr, index, randomIndex)
+        })
+        return arr
+    }
+
+    swap(arr, indexA, indexB) {
+        [arr[indexA], arr[indexB]] = [arr[indexB], arr[indexA]]
     }
 }
